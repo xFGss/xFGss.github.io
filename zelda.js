f52d0efbccf2734d47c564e4a8aa9d36b1c4d1f4
xfGs <113046290+xFGss@users.noreply.github.com>
2025-04-14 11:53:56 +0100

    Delete zelda.js

--- zelda.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.querySelector(".hamburger-menu");
-    const menu = document.querySelector(".menu");
-
-    menuButton.addEventListener("click", function () {
-        menu.classList.toggle("show");
-    });
-});
